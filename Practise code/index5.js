088e436d743ac722355284d16e112af9d7f4945f
cseatikur <atikurra00005@gmail.com>
2024-12-21 15:33:12 +0600

    generic

---
+++ Practise code/index5.js
@@ -0,0 +1,6 @@
+function printUserInfo1(userid) {
+    console.log("user id ".concat(userid));
+}
+printUserInfo1(true);
+printUserInfo1(1010);
+printUserInfo1("1010");
---
+++ Practise code/index5.ts
@@ -0,0 +1,6 @@
+function printUserInfo1 <X>(userid: X){
+    console.log(`user id ${userid}`);
+}
+printUserInfo1(true)
+printUserInfo1(1010)
+printUserInfo1("1010")
